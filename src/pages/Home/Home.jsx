6f698dbb1f6a174960c98b2961c9f9de33661218
touhidcodes <touhidcodes@gmail.com>
2024-10-19 09:00:53 +0600

    fix: homepage color

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,10 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 
 const Home = () => {
   return (
-    <div className="bg-[#171717] pt-10 ">
+    <div className="pt-10 ">
       <div className="grid grid-cols-7 max-w-screen-xl mx-auto">
         <div className="col-span-2 bg-yellow-500 rounded-md p-10">
-          <div className="border-2 rounded-md border-[#171717]">
+          <div className="border-2 rounded-md">
             <img
               src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1385&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
@@ -21,9 +21,7 @@ const Home = () => {
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-4xl uppercase text-black font-semibold">
-              About
-            </h1>
+            <h1 className="text-4xl uppercase  font-semibold">About</h1>
             <p className="m-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Perferendis nemo voluptatum, vel, est mollitia accusantium vero
