fdb54b242161539148d8b9cc331e3be413e0dc32
touhidcodes <touhidcodes@gmail.com>
2024-10-23 22:09:18 +0600

    feat: skills page added

---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PageTitle from "../../components/PageTitle/PageTitle";
+
+const Skills = () => {
+  return (
+    <div className="pt-10 ">
+      <div className="grid grid-cols-7 max-w-screen-xl mx-auto">
+        <div className="col-span-2 bg-yellow-500 rounded-md p-10">
+          <div className="border-2 rounded-md">
+            <img
+              src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1385&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="w-56 h-56 rounded-full m-auto mt-6"
+            />
+            <h1 className="uppercase text-center mt-10 text-2xl font-bold ">
+              M. saffar Creations
+            </h1>
+            <div className="text-xl font-semibold my-5 text-right mr-12">
+              <h4>Graphic Designer &</h4>
+              <h4>Calligrapher</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
